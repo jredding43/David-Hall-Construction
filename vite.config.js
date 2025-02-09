0e37657b0c81be5ae18ace337619eb1b99988173
Jack Redding <jredding2543@gmail.com>
2025-02-08 17:51:24 -0800

    Updated vite.config.js for GitHub Pages deployment

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  base: "https://jredding43.github.io/David-Hall-Construction", 
+  plugins: [react()],
+});

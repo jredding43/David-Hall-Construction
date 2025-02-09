182eb0554474a383f352b35da47c152889903291
Jack Redding <jredding2543@gmail.com>
2025-02-08 17:54:20 -0800

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "https://jredding43.github.io/David-Hall-Construction", 
+  base: "/David-Hall-Construction", 
   plugins: [react()],
 });

cb8c6e22395cd916087e833cd2ddae7d49f1a8b5
Jack Redding <jredding2543@gmail.com>
2025-02-08 18:03:24 -0800

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/David-Hall-Construction", 
+  base: "/David-Hall-Construction/", 
   plugins: [react()],
 });

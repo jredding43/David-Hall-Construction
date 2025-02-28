7472ee7fbff65096b051c4f0113e333b3e11ae39
Jack Redding <jredding2543@gmail.com>
2025-02-28 08:20:59 -0800

    Fixed Google Analytics tracking in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./components/Contact";
 import Services from "./components/Services";
